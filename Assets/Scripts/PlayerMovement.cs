1b1ea102e070e9562fcf018cb0d0dfd4ede854dd
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2023-01-11 14:50:48 +0100

    Player
    movement script för spelaren
    Skapade en scen för level 1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float playerSpeed = 1f;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //Referens till rigidbody2D
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Spelarens input uppdelat i x och y axeln
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //Spelarens hastighet som beror på playerSpeed och movement
+        rb.velocity = playerSpeed * Time.deltaTime * movement;
+    }
+}

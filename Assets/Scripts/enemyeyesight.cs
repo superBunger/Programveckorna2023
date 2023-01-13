c87745032f0faa476bd5abff56417f494aa92990
max burger <max.johanssonkinsella@elev.ga.lbs.se>
2023-01-13 14:46:39 +0100

    fienden fungerar

--- Assets/Scripts/enemyeyesight.cs
+++ Assets/Scripts/enemyeyesight.cs
@@ -6,7 +6,7 @@ public class enemyeyesight : MonoBehaviour
 {
 
     public bool detected = false;
-    
+    public bool colourChangeBack = false;
 
 
     // Start is called before the first frame update
@@ -21,18 +21,27 @@ public class enemyeyesight : MonoBehaviour
         
     }
 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && detected == true)
+        {
+            detected = false; // om man slutar bli sedd blir den falsk, och en kod för att ändra tillbaka färgen börjar - max och erik
+            colourChangeBack = true;
+            
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            detected = true; //om den ser fienden blir detected sann - max
-            StartCoroutine(detectedTimer());
+            detected = true; //om man blir sedd blir detected sann - max och erik
         }
 
-        IEnumerator detectedTimer()
-        {
-            yield return new WaitForSeconds(3);
-            detected = false;
-        }
+       
+    
+
     }
+
+
 }
--- Assets/Scripts/visionConeChangeColour.cs
+++ Assets/Scripts/visionConeChangeColour.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class visionConeChangeColour : MonoBehaviour
 {
-    Color redVision = new Color(0.8773585f, 0.2267888f, 0.2553718f, 0.972549f);
-    Color yellowVision = new Color(1f, 0.9592047f, 0.2962264f, 1f);
+    Color redVision = new Color(0.8773585f, 0.2267888f, 0.2553718f, 0.3f);
+    Color yellowVision = new Color(1f, 0.9592047f, 0.2962264f, 0.3f);
     SpriteRenderer sr;
     enemyeyesight ees;
     GameObject enemy;
 
+    bool routineStarted = false;
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -22,19 +24,36 @@ public class visionConeChangeColour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (ees.colourChangeBack == true && ees.detected == false)
+        {
+            if (routineStarted == false) //routine started används för att se till att bara en routine är active åt gången - max och erik
+            { 
+                StartCoroutine(redVisionTimer()); //startar timern för att ändra tillbaka färgen om man inte står i synen - m, e
+            }
+            
+        }
+        
         if (ees.detected == true)
         {
-            StartCoroutine(redVisionTimer());
-            sr.color = redVision; //om detected är sann så blir synen röd - max
+           
+            StopAllCoroutines();  //stoppar timern om man går in i grejen igen - m, e
+            routineStarted = false; //ändra routinestarted så att en ny routine får börja - m, e
+        }
+
+        if (ees.detected == true)
+        {
+            sr.color = redVision; //ändrar färg till röd - m, e
         }
     }
 
-    IEnumerator redVisionTimer()
+    IEnumerator redVisionTimer() //timer för att ändra färg tillbaka till gul - m, e
     {
-        print("coroutineStarted");
+        routineStarted = true; 
+        print("coroutine started");
         yield return new WaitForSeconds(3);
         sr.color = yellowVision;
-        
+        routineStarted = false;
+       
     }
 
 

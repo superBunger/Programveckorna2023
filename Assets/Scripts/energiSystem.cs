19fec2c2d5b97cb835fc2ee418d4201ebc4e57b9
max burger <max.johanssonkinsella@elev.ga.lbs.se>
2023-01-16 09:57:02 +0100

    lite grejer

--- Assets/Scripts/energiSystem.cs
+++ Assets/Scripts/energiSystem.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class energiSystem : MonoBehaviour
 {
+    int energyBar = 0;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "battery")
+        {
+            energyBar += 1;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {

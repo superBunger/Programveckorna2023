f3c3a2af6631052b8502909a810821664ee85c99
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2023-01-18 15:23:24 +0100

    Revert "Merge branch 'main' of https://github.com/superBunger/Programveckorna2023"
    This reverts commit cfd699e284060432d868d8a86c495b4b6bf28042, reversing
    changes made to dca6d47b7d131e01490c148c38a64e3930ecab45.

--- Assets/Scripts/BreakWall.cs
+++ Assets/Scripts/BreakWall.cs
@@ -8,7 +8,7 @@ public class BreakWall : MonoBehaviour
     {
         if (collision.gameObject.tag == "Juggernaut" || collision.gameObject.tag == "bomb")
         {
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/dogPathfinding.cs
+++ Assets/Scripts/dogPathfinding.cs
@@ -1,52 +1,28 @@
-using NavMeshPlus.Extensions;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using static UnityEngine.GraphicsBuffer; 
+using NavMeshPlus;
 
 public class dogPathfinding : MonoBehaviour
 {
-    NavMeshAgent agent;
+    
     [SerializeField]
     public Transform[] waypoint;
     bool detected;
-    bool reset = false;
-    
-
-    private void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        
-        agent.updateRotation = false; 
-        agent.updateUpAxis = false;
-    }
     // Start is called before the first frame update
     void Start()
     {
         print(waypoint.Length);
-        var agentPosition = agent.transform.position;
-        StartCoroutine("dogWalk");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (reset = true)
-        {
-            StartCoroutine("dogWalk");
-        }
- 
-    }
-
-    IEnumerator dogWalk(){ 
-    for (int i=0; i<waypoint.Length; i++)
-           {
-                transform.up = waypoint[i].position - transform.position;
-                while (agent.transform.position != waypoint[i].position)
-                agent.SetDestination(waypoint[i].position);
-           }
-        reset = true;
-        yield return null;
+        while (detected) {
+         for (int i=0; i < waypoint.Length; i++)
+            {
+             
+            }
+        } 
     }
 }

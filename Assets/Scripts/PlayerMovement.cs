875f520ac46626702bb60d60da36e599d62ee12d
max burger <max.johanssonkinsella@elev.ga.lbs.se>
2023-01-19 13:58:18 +0100

    provar detta

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,9 +9,7 @@ public class PlayerMovement : MonoBehaviour
     float boostspeed = 350f;
     float speedDuration = 2.5f;
     public bool speedBoostActive = false;
-    Vector2 movementLR = new Vector2(10, 0);
-    Vector2 movementUD = new Vector2(0, 10);
-    bool playerUpward;
+    Vector2 movement;
 
     public energiSystem es;
     public particlesystemscript pss;
@@ -42,13 +40,15 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-       
+        rb.velocity = playerSpeed * Time.deltaTime * movement.normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Spelarens input uppdelat i en horisontell och vertikal axel
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
 
        
         if (Input.GetKeyDown(KeyCode.Alpha1) && speedBoostActive == false && es.energyBar >= 1)
@@ -116,31 +116,19 @@ public class PlayerMovement : MonoBehaviour
         {
             bomb = Instantiate(bombPrefab, transform.position, transform.rotation);
             es.energyBar -= 4;
-            bomb.GetComponent<Animator>().SetTrigger("bombTime");  //spawnar en bomb, startar animationen och sedan tar bort bomben och väggen - max
+            bomb.GetComponent<Animator>().SetTrigger("bombTime");
             StartCoroutine(bombTimer());
         }
         
         IEnumerator bombTimer()
         {
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.85f);
+            Destroy(bomb);
             wallDestroy = FindObjectOfType<BreakWall>().gameObject;
             Destroy(wallDestroy);
-
-
         }
 
-
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.AddForce(movementUD * Time.deltaTime);
-            playerUpward = true;
-        }
-        if(Input.GetKeyUp(KeyCode.W))
-        {
-            rb.AddForce(-movementUD);
-            playerUpward = false;
-        }
-
+        
             
            
     }

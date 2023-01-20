a95c94ade29591fee6c67d50e524f212bc46755f
max burger <max.johanssonkinsella@elev.ga.lbs.se>
2023-01-20 11:09:43 +0100

    hej henry

--- Assets/Scripts/creepyLittleHead.cs
+++ Assets/Scripts/creepyLittleHead.cs
@@ -6,15 +6,49 @@ public class creepyLittleHead : MonoBehaviour
 {
     public int headNumber;
     public rotatingVision body;
-    public SpriteRenderer spriteRenderer;
-    public Sprite[] headPosition;
+    Animator animator;
+    
+    
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-       spriteRenderer.sprite = headPosition[headNumber];
+      if(body.gameObject.transform.rotation.z >= -20 && body.gameObject.transform.rotation.z < 25)
+      {
+            animator.SetInteger("owlStage", 0);
+      }
+
+        if (body.gameObject.transform.rotation.z >= 25 && body.gameObject.transform.rotation.z < 70)
+        {
+            animator.SetInteger("owlStage", 1);
+        }
+
+        if (body.gameObject.transform.rotation.z >= 70 && body.gameObject.transform.rotation.z < 115)
+        {
+            animator.SetInteger("owlStage", 2);
+        }
+        
+        if (body.gameObject.transform.rotation.z >= 115 && body.gameObject.transform.rotation.z < 160)
+        {
+            animator.SetInteger("owlStage", 3);
+        }
+
+        if (body.gameObject.transform.rotation.z >= 160 && body.gameObject.transform.rotation.z < 205)
+        {
+            animator.SetInteger("owlStage", 4);
+        }
+
+        if (body.gameObject.transform.rotation.z >= 205 && body.gameObject.transform.rotation.z < 250)
+        {
+            animator.SetInteger("owlStage", 5);
+        }
+
+        if (body.gameObject.transform.rotation.z >= 295 && body.gameObject.transform.rotation.z < 340)
+        {
+            animator.SetInteger("owlStage", 6);
+        }
     }
 }
--- Assets/Scripts/rotatingVision.cs
+++ Assets/Scripts/rotatingVision.cs
@@ -12,12 +12,7 @@ public class rotatingVision : MonoBehaviour
     public Quaternion minAngles;
     public Quaternion maxAngles;
     public bool whipIt;
-    public int headPosition;
-    public Transform head;
-    public float rotationTracker = 0;
-    public creepyLittleHead creepyLittle;
-
-
+    
     public particlesystemscript pss;
 
     private void Start()
@@ -35,17 +30,8 @@ public class rotatingVision : MonoBehaviour
         else if (peepin == false && setRotation == true && whipIt == false)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, minAngles, Time.deltaTime * rotation);
-            rotationTracker += Time.deltaTime * rotation;
-            if (rotationTracker >= 45)
-            {
-                creepyLittle.headNumber += 1;
-                rotationTracker -= 45;
-            }
-            else if (rotationTracker <= -45)
-            {
-                creepyLittle.headNumber -= 1;
-                rotationTracker += 45;
-            }
+            
+            
             if (transform.rotation == minAngles)
             {
                 whipIt = true;
@@ -55,17 +41,7 @@ public class rotatingVision : MonoBehaviour
         else if (peepin == false && setRotation == true && whipIt == true)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, maxAngles, Time.deltaTime * rotation);
-            rotationTracker += Time.deltaTime * rotation;
-            if (rotationTracker >= 45)
-            {
-                creepyLittle.headNumber += 1 ;
-                rotationTracker -= 45;
-            }
-            else if (rotationTracker <= -45)
-            {
-                creepyLittle.headNumber -= 1;
-                rotationTracker += 45;
-            }
+           
             if (transform.rotation == maxAngles)
             {
                 whipIt = false;

444c6183483337e1c0d7c9b43b43211cbcdcb83d
max burger <max.johanssonkinsella@elev.ga.lbs.se>
2023-02-01 10:08:44 +0100

    IVE INVENTED THE WHEEEEL

--- Assets/Scripts/creepyLittleHead.cs
+++ Assets/Scripts/creepyLittleHead.cs
@@ -4,54 +4,20 @@ using UnityEngine;
 
 public class creepyLittleHead : MonoBehaviour
 {
-    public int headNumber;
+   
     Animator animator;
-    public Transform GameObject;
+ 
     
     
     private void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponentInParent<Animator>();
     }
 
     private void Update()
     {
-        animator.SetFloat("owlRotation", gameObject.transform.localRotation.eulerAngles.z);
-        if (gameObject.transform.localRotation.eulerAngles.z >= -20 && gameObject.transform.localRotation.eulerAngles.z < 25)
-        {
-            animator.SetInteger("owlStage", 0);
-        }
-
-        if (gameObject.transform.localRotation.eulerAngles.z >= 25 && gameObject.transform.localRotation.eulerAngles.z < 70)
-        {
-            animator.SetInteger("owlStage", 1);
-        }
-
-        if (gameObject.transform.localRotation.eulerAngles.z >= 70 && gameObject.transform.localRotation.eulerAngles.z < 115)
-        {
-            animator.SetInteger("owlStage", 2);
-        }
-
-        if (gameObject.transform.localRotation.eulerAngles.z >= 115 && gameObject.transform.localRotation.eulerAngles.z < 160)
-        {
-            animator.SetInteger("owlStage", 3);
-        }
-
-        if (gameObject.transform.localRotation.eulerAngles.z >= 160 && gameObject.transform.localRotation.eulerAngles.z < 205)
-        {
-            animator.SetInteger("owlStage", 4);
-        }
-
-        if (gameObject.transform.localRotation.eulerAngles.z >= 205 && gameObject.transform.localRotation.eulerAngles.z < 250)
-        {
-            animator.SetInteger("owlStage", 5);
-        }
-
-        if (gameObject.transform.localRotation.eulerAngles.z >= 295 && gameObject.transform.localRotation.eulerAngles.z < 340)
-        {
-            animator.SetInteger("owlStage", 6);
-        }
-
-
+       
+        animator.SetFloat("owlRotation", gameObject.transform.localRotation.eulerAngles.z); //säger till animatorn hur mycket ugglan har roterat, animatorn ändrar sprite baserat på det - max
+        
     }
 }
